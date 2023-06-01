d5ed04dc8c4a1b343b681b5673c06561e82453bc
Leonardo Pacheco Dos Santos <l.pacheco@globant.com>
2023-06-01 17:50:25 -0300

    projeto

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -1,3 +1,29 @@
 const porta = 3003;
 
 const express = require('express');
+const app = express();
+const bancoDeDados = require("./bancoDeDados");
+
+app.get('/produtos', (req, res, next) => {
+    console.log('Middleware 1...');
+    next();
+});
+
+app.get('/produtos', (req, res) => {
+    res.send(bancoDeDados.getProdutos());
+});
+
+app.get('/produtos/:id', (req, res) => {
+    res.send(bancoDeDados.getProduto(req.params.id));
+});
+
+app.post('/produtos', (req, res) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto);
+});
+app.listen(porta, () => {
+    console.log(`Servidor rodando na porta ${porta}`);
+});
